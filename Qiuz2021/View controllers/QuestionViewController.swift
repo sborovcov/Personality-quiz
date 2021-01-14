5911b866a9770ca409ae45088e789e6c255f0901
Stas Borovcov <sborovcov@gmail.com>
2021-01-14 17:07:06 +0700

    Finished project

--- Qiuz2021/View controllers/QuestionViewController.swift
+++ Qiuz2021/View controllers/QuestionViewController.swift
@@ -8,22 +8,166 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet var singleButtons: [UIButton]!
+    @IBOutlet weak var singleStackView: UIStackView!
+    
+    @IBOutlet weak var multiplyStackView: UIStackView!
+    @IBOutlet var multiLabels: [UILabel]!
+    @IBOutlet var multiSwiches: [UISwitch]!
+    
+    @IBOutlet weak var rangeSlider: UISlider!
+    @IBOutlet weak var rangeStackView: UIStackView!
+    @IBOutlet var rangeLabels: [UILabel]!
+    
+    @IBOutlet weak var quiestionProgressView: UIProgressView!
+    
+    private var answersChoosen = [Answer](){
+        didSet{
+            //print(answersChoosen)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private var currentAnswers: [Answer]{
+        currentQuestion.answers
     }
-    */
+    
+    private var currentQuestion: Question{
+        Question.all[questionIndex]
+    }
+            
+    var questionIndex = 0
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rangeSlider.maximumValue = 0.99999
+        updateUI()
+    }
+    
+    // функция для изменения интерфейса как рекомендует apple
+    func updateUI() {
+        
+        // сначала эти функции были снаружи updateUI() и им был недоступен answers, но мы можем сделать функцию в функции и
+        // тогда вложенным функциям будут доступны переменные updateUI(), но сами функции не будут видны вне updateUI()
+        func updateSingleStack(){
+            singleStackView.isHidden = false
+            
+            for (index, button) in singleButtons.enumerated(){
+                button.setTitle("", for: [])
+                button.tag = index // если поменяем количество кнопок, то в tag  точно будет значение, а его мы используем при нажатии на кнопку для запоминания ответа пользователя
+            }
+            
+            // здесь может быть ошибка, если ответов меньше, чем кнопок
+//            for (index, button) in singleButtons.enumerated() {
+//                button.setTitle(answers[index].text, for: [])
+//            }
+            // поэтому переделаем на использование функции zip, которая из двух последовательностей выбирает пары
+            for (button, answer) in zip(singleButtons, currentAnswers){
+                button.setTitle(answer.text, for: [])
+            }
+        }
+        
+        func updatemultiplyStackView(){
+            multiplyStackView.isHidden = false
+            
+            for label in multiLabels{
+                label.text = ""
+            }
+            
+            for (label, answer) in zip(multiLabels, currentAnswers){
+                label.text = answer.text
+            }
+        }
+        
+        func updaterangeStackView(){
+            rangeStackView.isHidden = false
+            
+            rangeLabels.first?.text = currentAnswers.first?.text
+            rangeLabels.last?.text = currentAnswers.last?.text
+        }
+        
+        // можно задать свойство по отдельности
+//        singleStackView.isHidden = true
+//        multiplyStackView.isHidden = true
+//        rangeStackView.isHidden = true
+        
+        // можно задать свойство в цикле
+        for stackView in [singleStackView, multiplyStackView, rangeStackView]{
+            stackView?.isHidden = true
+        }
+        
+        // определим тип текущего вопроса
+        //let currentQuestion = Question.all[questionIndex]
+        //let currentAnswers = question.answers
+        let totalProgress = Float(questionIndex) / Float(Question.all.count)
+        
+        navigationItem.title = "Вопрос №\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        quiestionProgressView.setProgress(totalProgress, animated: true)
+        
+        
+        // нарисуем на форме элементы, соответствующие типу вопроса
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack()
+        case .multiply:
+            updatemultiplyStackView()
+        case .range:
+            updaterangeStackView()
+        }
+        
+    }
 
+    func nextQuestion(){
+        // так мы делали что бы просто ходить по окнам зацикленно, но нам недо на последнем вопросе перейти к показу результата
+        //questionIndex = (questionIndex + 1) % Question.all.count
+        questionIndex += 1
+        if questionIndex < Question.all.count{
+            updateUI()
+        }else{
+            // вызываем view с переданным идентификатором(задается в свойствах segue - перехода между формами. Вот те стрелочки в main.storyboard)
+            performSegue(withIdentifier: "Result segue", sender: nil)
+        }
+    }
+    
+    @IBAction func singleButtonPressed(_ sender: UIButton) {
+        let answers = Question.all[questionIndex].answers
+        let index = sender.tag
+        
+        guard 0 <= index && index < answers.count else{
+            return
+        }
+        
+        let answer = answers[index]
+        answersChoosen.append(answer)
+        
+        nextQuestion()
+    }
+ 
+    @IBAction func multiButtonPressed(){
+        for (index, multiSwich) in multiSwiches.enumerated(){
+            if multiSwich.isOn && index < currentAnswers.count {
+                let answer = currentAnswers[index]
+                answersChoosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangeButtonPressed(){
+        let index = Int(round(rangeSlider.value * Float(currentAnswers.count - 1)))
+        if index < currentAnswers.count {
+            let answer = currentAnswers[index]
+            answersChoosen.append(answer)
+        }
+        
+        nextQuestion()
+    }
+    
+    // этот экшн сделан от стрелочки перехода между формами и заменен инициализатор формы на наш с двумя параметрами
+    @IBSegueAction func resultSegue(_ coder: NSCoder) -> ResultsViewController? {
+        return ResultsViewController(coder: coder, answersChoosen)
+    }
 }
--- Qiuz2021/View controllers/ResultsViewController.swift
+++ Qiuz2021/View controllers/ResultsViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    let answers: [Answer]
+    
+    // все что касается coder: NSCoder и required init?(coder: NSCoder) объясняется в модуле по Quiz в части 16
+    init?(coder: NSCoder, _ answers: [Answer]){
+        self.answers = answers
+        super.init(coder: coder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    func calculatePersonalResult(){
+        
+        let frequencyOfAnswers = answers.reduce(into: [:]) { counts, answer in
+            //counts[answer.type] может быть nil и тогда его нельзя сложить с 1, поэтому можно написать проверку
+            //counts[answer.type] = (counts[answer.type] ?? 0) + 1
+            // но можно это же самое сделать так
+            counts[answer.type, default: 0] += 1
+        }
+        //агрументы можно заменять на $ с номером и тогда сократить эту строку до(синтаксический сахар)
+        //let frequencyOfAnswersSorted = frequencyOfAnswers.sorted { pair1, pair2 in pair1.value > pair2.value }
+        let frequencyOfAnswersSorted = frequencyOfAnswers.sorted { $0.value > $1.value }
+        //print(frequencyOfAnswersSorted)
+        let mostCommonAnswers = frequencyOfAnswersSorted.first!.key
+        UpdateUI(with: mostCommonAnswers)
+    }
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // уберем на этом view стандартную кнопку возврата на предыдущий экран
+        navigationItem.hidesBackButton = true
+        calculatePersonalResult()
+    }
+    
+    func UpdateUI(with animal: AnimalType){
+        //rawValue - без него вывелось бы слово, соответсвующее картинке, а так непосредственно само значение
+        animalLabel.text = "Вы - это \(animal.rawValue)"
+        descriptionLabel.text = animal.definition
+    }
+    
 }
